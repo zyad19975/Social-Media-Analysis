b2eb90e9423ed8c7ab828243ce209763389c9b75
samirMossad1 <samirmossad6@gmail.com>
2019-06-24 07:11:45 +0200

    added the constructor and the read functions

---
+++ graph.c
@@ -0,0 +1,50 @@
+
+#include "graph.h"
+
+
+Graph::Graph(int number_nodes)
+{
+
+    adj_list.resize(number_nodes);
+    nodes_edges.resize(number_nodes,0);
+
+
+}
+
+void Graph::read_File(ifstream& myFile)
+{
+
+    int node,adjacent_node,edge_weight;
+
+    while(!(myFile.eof()))
+    {
+        myFile>>node>>adjacent_node>>edge_weight;
+
+        adj_list[node].push_back({adjacent_node,edge_weight});
+        adj_list[adjacent_node].push_back({node,edge_weight});
+        nodes_edges[node]++;
+        nodes_edges[adjacent_node]++;
+    }
+
+
+}
+
+void Graph::read_File(int number_edges)
+{
+
+    int node,adjacent_node,edge_weight;
+
+    while(number_edges--)
+    {
+        cin>>node>>adjacent_node>>edge_weight;
+
+        adj_list[node].push_back({adjacent_node,edge_weight});  
+        adj_list[adjacent_node].push_back({node,edge_weight});  
+        nodes_edges[node]++;
+        nodes_edges[adjacent_node]++;
+        
+    }
+
+}
+
+
